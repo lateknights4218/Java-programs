dc36b3365fc02b847a0e90c4559873ce1d1053e7
Chris Skane <the_epic_battlefielder@live.com>
2016-12-26 23:44:43 -0500

    Created test files

---
+++ Ours/native.java
@@ -0,0 +1,44 @@
+package org.firstinspires.ftc.robotcontroller.Ours;
+
+import java.io.*
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+
+// Program to test if we can execute native code on phone
+
+@TeleOp(name="Native",group = "test")
+public class native extends OpMode{
+
+	Runtime rt = null;
+	Process p = null;
+	BufferedReader stdIn;
+
+	public void init(){
+		try{
+		  rt = Runtime.getRuntime();
+		}catch(Exception e){}
+	}
+	public void init_loop(){}
+
+	public void start(){}
+
+	int count=0;
+	String s = null;
+	public void loop(){
+		try{
+		  p = rt.exec(new String[]{"./hello",Integer.toString(count)});// Will fail. Need to compile with NDK
+		  stdIn = new BufferedReader(new InputStreamReader(p.getInputStream()));
+		  
+		  s = stdIn.readLine();
+		}catch(Exception e){}
+
+		if(s != null) telemetry.addData("Output","%s",s);
+
+		count++;
+	}
+
+	public void stop(){}
+}
